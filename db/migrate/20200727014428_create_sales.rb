37eab97c2245f8c28cbb8c0e2e867bd911e1f454
Jonas Elan <jonas.elan@gmail.com>
2020-08-01 12:20:30 -0300

    Currency values to decimal with precision

--- db/migrate/20200727014428_create_sales.rb
+++ db/migrate/20200727014428_create_sales.rb
@@ -2,11 +2,11 @@ class CreateSales < ActiveRecord::Migration[6.0]
   def change
     create_table :sales do |t|
       t.references :deliveryman, null: true, foreign_key: { to_table: :deliverymen }
-      t.decimal :value
-      t.decimal :charge
+      t.decimal :value, precision: 5, scale: 2
+      t.decimal :charge, default: 0, precision: 5, scale: 2
       t.string :payment_method
       t.string :delivery_method
-      t.decimal :delivery_fee
+      t.decimal :delivery_fee, precision: 5, scale: 2
 
       t.timestamps
     end
--- db/migrate/20200727014617_create_expenses.rb
+++ db/migrate/20200727014617_create_expenses.rb
@@ -1,7 +1,7 @@
 class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
-      t.integer :value
+      t.integer :value, :decimal, precision: 5, scale: 2
       t.integer :kind #
 
       t.timestamps
