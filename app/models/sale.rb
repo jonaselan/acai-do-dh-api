c6fc37e00b28a262e9294dadec353fa0ef3dc9fe
Jonas Elan <jonas.elan@gmail.com>
2020-08-09 18:48:18 -0300

    Specific sales better with  enums

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,3 +1,7 @@
 class Sale < ApplicationRecord
+  enum payment_method: { cash: 'cash', credit_card: 'credit_card' }
+  enum delivery_method: { delivery: 'delivery', in_store: 'in_store' }
+
+
   belongs_to :deliveryman
 end
