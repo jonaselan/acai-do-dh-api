a89986c35bd5539f70d6bbe85cb81f1b227f970e
Jonas Elan <jonas.elan@gmail.com>
2020-09-13 21:12:48 -0300

    Setup i18n to pt-br

--- config/application.rb
+++ config/application.rb
@@ -24,14 +24,10 @@ module AcaiDoDhApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.i18n.default_locale = :"pt-BR"
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.time_zone = 'UTC'
+
     config.api_only = true
   end
 end
